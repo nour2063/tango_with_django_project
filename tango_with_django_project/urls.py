f1f23834cb374ac8764924c49975dbed9125ec00
“Nour <2660369s@student.gla.ac.uk>
2023-02-07 17:31:43 +0000

    Chapter 3

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Chapter 3
+from rango import views
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from rango import views
+
 urlpatterns = [
+    path('', views.index, name="index"),
+    path('rango/', include('rango.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
+
