2ddbb12c6e1e1fb63ed6e2f2f97abb6532f73412
“Nour <2660369s@student.gla.ac.uk>
2023-02-07 21:16:51 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Chapter 5
+from django.contrib import admin
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
