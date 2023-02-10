d0ff7373860a2efaf004b69700258377910f5b69
“Nour <2660369s@student.gla.ac.uk>
2023-02-10 18:14:31 +0000

    Chapter 9

--- rango/models.py
+++ rango/models.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
-    NAME_MAX_LENGTH = 128
-
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    # constants:
+    MAX_NAME_LENGTH = 128
+    # instance fields:
+    name = models.CharField(max_length=MAX_NAME_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -15,20 +17,33 @@ class Category(models.Model):
         super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
 
 
 class Page(models.Model):
-    TITLE_MAX_LENGTH = 128
-    URL_MAX_LENGTH = 200
-
+    # constants:
+    MAX_TITLE_LENGTH = 128
+    MAX_URL_LENGTH = 200
+    # instance fields:
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=TITLE_MAX_LENGTH)
-    url = models.URLField()
+    title = models.CharField(max_length=MAX_TITLE_LENGTH)
+    url = models.URLField(max_length=MAX_URL_LENGTH)
     views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
